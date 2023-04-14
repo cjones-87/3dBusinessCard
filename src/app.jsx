36cdb62e464860ca0fa757e2bc4526cc10ae9279
CJ <cliftonjones.87@gmail.com>
2023-04-14 02:53:21 -0400

    chore: served up routes to app, removed placeholder

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
+import NavigationRoutes from './navigation/NavigationRoutes';
 import Spinner from '../misc/Spinner';
 
 export const Loading = () => (
@@ -18,7 +19,7 @@ const App = () => {
   return (
     <>
       <Suspense fallback={<Loading />}>
-        <div>hello world</div>
+        <NavigationRoutes />
       </Suspense>
     </>
   );
